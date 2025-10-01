ea3dd1240f876701a4473356eb337adb8be39fa9
Ramya Muttireddy <ramyamuttireddy@gmail.com>
2025-10-01 23:33:35 +0530

    commitw orks

--- src/Components/Work/About.jsx
+++ src/Components/Work/About.jsx
@@ -3,7 +3,7 @@ import ScrollVelocity from "./ScrollVelocity";
 export default function About() {
   return (
     <div className=" h-[60vh] flex flex-col gap-20 bg-white justify-center items-end">
-      <div className="border-b border-black mb-4 w-[80%] flex items-end">
+      <div className="border-t border-black mb-4 w-[80%] flex items-end">
         <div className="text-black text-2xl font-bold px-4 py-2  flex">
           Explore more
         </div>
