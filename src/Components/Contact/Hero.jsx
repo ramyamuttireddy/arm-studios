0e50d2c269340cdff853f930fe8706df66334d55
Ramya Muttireddy <ramyamuttireddy@gmail.com>
2025-10-02 01:44:14 +0530

    commit

--- src/Components/Contact/Hero.jsx
+++ src/Components/Contact/Hero.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 export default function Hero() {
   return (
     <div>
-        <div className="bg-black text-white">
+        <div className="bg-black text-white h-screen w-full mx-auto">
               <div className="w-[90%] xl:w-[85%] m-auto">
                 {/* hero Section */}
-                  <div className="pt-20 pb-14 2xl:pt-36 2xl:pb-28 3xl:pt-96 3xl:pb-72">
+                  <div className="pt-60 pb-20 2xl:pt-50 2xl:mb-20 3xl:pt-[600px] 3xl:pb-72 ">
                        {/* say hii div */}
                            <div className="flex flex-col justify-center items-center gap-6 md:gap-3 lg:justify-start lg:items-start">
                                {/* say hii and cricle div */}
                                  <div className="flex flex-col gap-6 md:flex-row md:justify-end md:items-end lg:gap-36 2xl:gap-52 1xl:gap-64">
                                      {/* say hii text div */}
-                                      <div className="text-4xl font-medium md:text-6xl lg:text-7xl xl:text-[84px] 2xl:text-[116px] 1xl:text-9xl 3xl:text-[200px]">
+                                      <div className="text-4xl font-medium md:text-6xl lg:text-[60px] xl:text-[70px] 2xl:text-[96px] 1xl:text-9xl 3xl:text-[200px]">
                                          <div className="flex justify-end pr-4 ">
                                              <div>SAY HI TO</div>
                                          </div>
@@ -21,23 +21,23 @@ export default function Hero() {
                                        {/* say hii text div */}
                                        {/* blue circle div */}
                                          <div className="flex justify-center">
-                                            <div className="bg-[#0100FC] size-44 lg:size-56 xl:size-64 2xl:size-[380px] 1xl:size-[500px] rounded-full text-center flex justify-center items-center">
+                                            <div className="bg-[#0100FC] size-44 lg:size-56 xl:size-50 2xl:size-[250px] 1xl:size-[500px]  3xl:size-[500px] rounded-full text-center flex justify-center items-center shadow-2xl">
                                               <div className="text-xl font-medium xl:text-2xl 1xl:text-4xl 3xl:text-6xl">Contact us</div>
                                            </div>
                                         </div>   
                                        {/* blue circle div */}
                                  </div>
-                                 <div className="font-medium text-2xl md:text-6xl lg:text-7xl xl:text-[84px] 2xl:text-[116px] 1xl:text-9xl 3xl:text-[200px]">info@armstudios.design</div>
+                                 <div className="font-medium text-2xl md:text-6xl lg:text-[60px] xl:text-[70px] 2xl:text-[96px] 1xl:text-9xl 3xl:text-[200px]">info@armstudios.design</div>
                                 {/* say hii and cricle div */}
                            </div>
                        {/* say hii div */}
                         {/* email information div */}
-                           <div className="pt-20 lg:pt-28 2xl:pr-28 1xl:pr-16 1xl:pt-36">
-                               <div className="md:w-[70%] m-[0px_0px_0px_auto] xl:w-[60%] 2xl:w-[60%] 2xl:m-[0px_0px_0px_auto] 1xl:w-[45%] 1xl:m-[0px_auto] 3xl:w-[40%] ">
+                           <div className="pt-20 lg:pt-28 2xl:pr-28 1xl:pr-16 1xl:pt-36 pb-40">
+                               <div className="md:w-[70%] m-[0px_0px_0px_auto] xl:w-[60%] 2xl:w-[60%]  2xl:m-[0px_0px_0px_auto] 1xl:w-[45%] 1xl:m-[0px_auto] 3xl:w-[40%] ">
                                   <div className="">
                                       <div className="flex justify-between">
                                          <div className="text-base font-medium flex flex-col gap-6
-                                                          md:text-xl xl:gap-10 2xl:text-2xl 3xl:text-4xl 3xl:gap-[74px]">
+                                                          md:text-md xl:gap-10 2xl:text-xl 3xl:text-4xl 3xl:gap-[74px]">
                                              <div>General</div>
                                               <div>Projects</div>
                                               <div>Press</div>
@@ -45,7 +45,7 @@ export default function Hero() {
                                               <div>Phone</div>
                                          </div>
                                           <div className="text-lg font-medium flex flex-col gap-5
-                                                           md:text-2xl xl:gap-9 2xl:text-3xl 3xl:text-5xl 3xl:gap-16">
+                                                           md:text-2xl xl:gap-9 2xl:text-2xl 3xl:text-5xl 3xl:gap-16 ">
                                                <div>info@armstudios.design</div>
                                                <div>inquiry@armstudios.design</div>
                                                 <div>press@armstudios.design</div>
--- src/Components/Contact/contactSection.jsx
+++ src/Components/Contact/contactSection.jsx
@@ -2,50 +2,63 @@ import React from "react";
 
 export default function ContactSection() {
   return (
-    <section
-      className="relative w-full min-h-screen bg-no-repeat bg-cover bg-center"
-      style={{
-        backgroundImage: "url('/assets/img/contactus-bg-images.jpg')",
-      }}
-    >
-      <div className="w-[90%] m-auto">
+    <section className="relative w-full min-h-screen">
+      {/* Background Image */}
+      <div
+        className="absolute inset-0 bg-no-repeat bg-cover bg-center"
+        style={{
+          backgroundImage: "url('/assets/img/contactus-bg-images.jpg')",
+        }}
+      ></div>
+
+      {/* Overlay for opacity */}
+      <div className="absolute inset-0 bg-black/50"></div>
+
+      {/* Content */}
+      <div className="relative w-[90%] m-auto z-10">
         {/* Top Border & Title */}
-        <div className="pt-14 pb-14 lg:pt-[75px] lg:pb-[75px] xl:pt-[90px] xl:pb-[90px] 3xl:pt-32 3xl:pb-32">
+        <div className="pt-14 pb-14 lg:pt-[75px] lg:pb-[75px] xl:pt-[90px] xl:pb-[90px] 3xl:pt-40 3xl:pb-40">
           <div className="flex justify-end">
             <div className="border-t w-[70%] border-black">
-              <div className="pt-5 text-xl md:text-2xl">Contact us</div>
+              <div className="pt-5 text-xl md:text-2xl 2xl:text-3xl 3xl:text-6xl text-white">
+                Contact us
+              </div>
             </div>
           </div>
 
           {/* Main Contact Text */}
-          <div className="text-black flex flex-col md:gap-3 font-medium text-4xl pt-16 md:pt-20 md:text-5xl xl:text-7xl 2xl:text-8xl 3xl:text-[140px]">
-            <div className="pl-10 md:pl-36 xl:pl-72 2xl:pl-[450px]">HELLO,</div>
+          <div className="flex flex-col md:gap-3 font-medium text-4xl md:text-5xl xl:text-7xl 2xl:text-8xl 3xl:text-[200px] pt-16 md:pt-20 3xl:pt-40 text-white">
+            <div className="pl-10 md:pl-36 xl:pl-72 2xl:pl-[450px] 3xl:pl-[600px]">
+              HELLO,
+            </div>
             <div className="flex flex-col md:gap-3">
               <div>
                 MY NAME IS
                 <div className="flex justify-end">
-                  <div className="border-b w-[60%] md:w-[50%] border-black"></div>
+                  <div className="border-b w-[60%] md:w-[50%] 3xl:w-[40%] border-white"></div>
                 </div>
               </div>
               <div>
                 HERE IS MY EMAIL
                 <div className="flex justify-end">
-                  <div className="border-b w-[40%] md:w-[30%] border-black"></div>
+                  <div className="border-b w-[40%] md:w-[30%] 3xl:w-[25%] border-white"></div>
                 </div>
               </div>
               <div>
                 I’M LOOKING FOR
                 <div className="flex justify-end">
-                  <div className="border-b w-[50%] md:w-[40%] border-black"></div>
+                  <div className="border-b w-[50%] md:w-[40%] 3xl:w-[30%] border-white"></div>
                 </div>
               </div>
             </div>
           </div>
 
           {/* Send Button */}
-          <div className="flex justify-center mt-16">
-            <button className="flex justify-center items-center bg-[#0100FC] w-44 h-44 md:w-52 md:h-52 rounded-full hover:bg-blue-500 transition duration-300">
-              <span className="text-lg md:text-2xl font-medium text-white">Send</span>
+          <div className="flex justify-center mt-16 3xl:mt-32">
+            <button className="flex justify-center items-center bg-[#0100FC] rounded-full w-44 h-44 md:w-52 md:h-52 2xl:w-60 2xl:h-60 3xl:w-[500px] 3xl:h-[500px] hover:bg-blue-500 transition duration-300 shadow-xl">
+              <span className="text-lg md:text-2xl 2xl:text-3xl 3xl:text-6xl font-medium text-white">
+                Send
+              </span>
             </button>
           </div>
         </div>
