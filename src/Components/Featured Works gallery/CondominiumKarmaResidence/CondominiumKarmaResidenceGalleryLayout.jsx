e45f3e89030451500c8f708089db82d314961a5e
Ramya Muttireddy <ramyamuttireddy@gmail.com>
2025-10-03 20:43:37 +0530

    commit

--- src/Components/Featured Works gallery/CondominiumKarmaResidence/CondominiumKarmaResidenceGalleryLayout.jsx
+++ src/Components/Featured Works gallery/CondominiumKarmaResidence/CondominiumKarmaResidenceGalleryLayout.jsx
@@ -2,34 +2,20 @@ import React, { useRef, useState, useEffect } from "react";
 import gsap from "gsap";
 import { SlArrowRight, SlArrowLeft } from "react-icons/sl";
 
-import img1 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 1.png"
-import img2 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 2.png"
-import img3 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 3.png"
-import img4 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 4.png"
-import img5 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 5.png"
-import img6 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 6.png"
-import img7 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 7.png"
-import img8 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 8.png"
-import img9 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 9.png"
-import img10 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 10.png"
-import img11 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 11.png"
-import img12 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 12.png"
-import img13 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 13.png"
+import img1 from "../../../../public/assets/PROJECTSALLPROJECTS/Condominium Karma Residence indonesia/PNG image 1.png"
+import img2 from "../../../../public/assets/PROJECTSALLPROJECTS/Condominium Karma Residence indonesia/PNG image 2.png"
+import img3 from "../../../../public/assets/PROJECTSALLPROJECTS/Condominium Karma Residence indonesia/PNG image 3.png"
+import img4 from "../../../../public/assets/PROJECTSALLPROJECTS/Condominium Karma Residence indonesia/PNG image 4.png"
+import img5 from "../../../../public/assets/PROJECTSALLPROJECTS/Condominium Karma Residence indonesia/PNG image 5.png"
+
 
 const slideData = [
   {id:1, img:img1},
   {id:2, img:img2},
   {id:3, img:img3},
   {id:4, img:img4},
-   {id:5, img:img5},
-  {id:6, img:img6},
-  {id:7, img:img7},
-  {id:8, img:img8},
-   {id:9, img:img9},
-  {id:10, img:img10},
-  {id:11, img:img11},
-  {id:12, img:img12},
-   {id:13, img:img13},
+  {id:6, img:img5},
+  
 ];
 
 export default function CondominiumKarmaResidenceGalleryLayout() {
