6e70b17c642e7ed9766b2907f5f6d1ac6bd3378a
Ramya Muttireddy <ramyamuttireddy@gmail.com>
2025-10-03 21:02:23 +0530

    commit

--- src/Components/Gallery/Villa22/Villa22GalleryLayout.jsx
+++ src/Components/Gallery/Villa22/Villa22GalleryLayout.jsx
@@ -2,34 +2,34 @@ import React, { useRef, useState, useEffect } from "react";
 import gsap from "gsap";
 import { SlArrowRight, SlArrowLeft } from "react-icons/sl";
 
-import img1 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 1.png"
-import img2 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 2.png"
-import img3 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 3.png"
-import img4 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 4.png"
-import img5 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 5.png"
-import img6 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 6.png"
-import img7 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 7.png"
-import img8 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 8.png"
-import img9 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 9.png"
-import img10 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 10.png"
-import img11 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 11.png"
-import img12 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 12.png"
-import img13 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 13.png"
+import img1 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 1.png";
+import img2 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 2.png";
+import img3 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 3.png";
+import img4 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 4.png";
+import img5 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 5.png";
+import img6 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 6.png";
+import img7 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 7.png";
+import img8 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 8.png";
+import img9 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 9.png";
+import img10 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 10.png";
+import img11 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 11.png";
+import img12 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 12.png";
+import img13 from "../../../../public/assets/PROJECTSALLPROJECTS/Villa 22/PNG image 13.png";
 
 const slideData = [
-  {id:1, img:img1},
-  {id:2, img:img2},
-  {id:3, img:img3},
-  {id:4, img:img4},
-   {id:5, img:img5},
-  {id:6, img:img6},
-  {id:7, img:img7},
-  {id:8, img:img8},
-   {id:9, img:img9},
-  {id:10, img:img10},
-  {id:11, img:img11},
-  {id:12, img:img12},
-   {id:13, img:img13},
+  { id: 1, img: img1 },
+  { id: 2, img: img2 },
+  { id: 3, img: img3 },
+  { id: 4, img: img4 },
+  { id: 5, img: img5 },
+  { id: 6, img: img6 },
+  { id: 7, img: img7 },
+  { id: 8, img: img8 },
+  { id: 9, img: img9 },
+  { id: 10, img: img10 },
+  { id: 11, img: img11 },
+  { id: 12, img: img12 },
+  { id: 13, img: img13 },
 ];
 
 export default function Villa22GalleryLayout() {
@@ -89,16 +89,17 @@ export default function Villa22GalleryLayout() {
   };
 
   return (
-    <section className="relative w-full h-[90vh] bg-black text-white flex flex-col items-center justify-center">
+    <section className="relative w-full h-screen bg-black text-white flex flex-col items-center justify-center">
       {/* Main Slides */}
       <div className="relative w-full aspect-video overflow-hidden">
         {slideData.map((slide, i) => (
           <div
             key={slide.id}
             ref={(el) => (slidesRef.current[i] = el)}
-            className="absolute inset-0 object-fit  bg-center will-change-transform w-full h-full bg-no-repeat"
+            className="absolute inset-0 w-full h-full bg-center bg-no-repeat"
             style={{
               backgroundImage: `url(${slide.img})`,
+              backgroundSize: "contain", // 👈 image full ga kanipistundi
               opacity: i === current ? 1 : 0,
             }}
           />
@@ -129,7 +130,7 @@ export default function Villa22GalleryLayout() {
           >
             <div
               ref={(el) => (thumbsRef.current[i] = el)}
-              className="w-20 h-12 sm:w-24 sm:h-16 md:w-20 md:h-20 3xl:w-60 3xl:h-60 bg-cover bg-center transition-transform transition-opacity"
+              className="w-20 h-12 sm:w-24 sm:h-16 md:w-10 md:h-10 3xl:w-60 3xl:h-60 bg-cover bg-center transition-transform transition-opacity"
               style={{ backgroundImage: `url(${slide.img})` }}
             />
             <div
