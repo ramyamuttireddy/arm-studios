3beba3366cb441cc9670a6315940d414728eba16
Ramya Muttireddy <ramyamuttireddy@gmail.com>
2025-10-05 08:38:14 +0530

    commit

--- src/Components/Gallery/Private Villa/PrivateVillaGalleryLayout.jsx
+++ src/Components/Gallery/Private Villa/PrivateVillaGalleryLayout.jsx
@@ -24,18 +24,18 @@ import img18 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa U
 import img19 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 19.png"
 import img20 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 20.png"
 import img21 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 21.png"
-import img22 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 8.png"
-import img23 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 9.png"
-import img24 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 10.png"
-import img25 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 11.png"
-import img26 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 12.png"
-import img27 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 13.png"
-import img28 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 14.png"
-import img29 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 15.png"
-import img30 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 16.png"
-import img31 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 17.png"
-import img32 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 18.png"
-import img33 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 19.png"
+import img22 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 22.png"
+import img23 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 23.png"
+import img24 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 24.png"
+import img25 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 25.png"
+import img26 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 26.png"
+import img27 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 27.png"
+import img28 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 28.png"
+import img29 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 29.png"
+import img30 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 30.png"
+import img31 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 31.png"
+import img32 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 32.png"
+import img33 from "../../../../public/assets/PROJECTSALLPROJECTS/Private Villa USA/PNG image 33.png"
 
 
 
@@ -61,18 +61,18 @@ const slideData = [
   {id:19, img:img19},
   {id:20, img:img20},
    {id:21, img:img21},
-    {id:22, img:img10},
-  {id:23, img:img11},
-  {id:24, img:img12},
-   {id:25, img:img13},
-   {id:26, img:img14},
-  {id:27, img:img15},
-  {id:28, img:img16},
-   {id:29, img:img17},
-  {id:30, img:img18},
-  {id:31, img:img19},
-  {id:32, img:img20},
-   {id:33, img:img21},
+    {id:22, img:img22},
+  {id:23, img:img23},
+  {id:24, img:img24},
+   {id:25, img:img25},
+   {id:26, img:img26},
+  {id:27, img:img27},
+  {id:28, img:img28},
+   {id:29, img:img29},
+  {id:30, img:img30},
+  {id:31, img:img31},
+  {id:32, img:img32},
+   {id:33, img:img33},
 ];
 
 export default function PrivateVillaGalleryLayout() {
